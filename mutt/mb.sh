e11bc9d84455d2d4f7726350bbd8977993ce2866
Iarom Madden <future@iarom.org>
2023-07-16 23:10:59 +0100

    mutt: bug fix of new config form. fix file pointers

--- mutt/mb.sh
+++ mutt/mb.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-#a=~/.config/mutt/mailboxes.`basename $1`
 mdir="$1"
-prfx1="$2"
-prfx2="mailboxes "
-mdirs=$(find "$mdir" -maxdepth 1 -mindepth 1 -type d -printf "+'$prfx1%f' ")
-out=${prfx2}${mdirs}
-echo "$out"
+prefix="$2"
+_mdirs() { find "$mdir" -maxdepth 1 -mindepth 1 -type d -printf "+'$prefix%f' "; }
+echo "mailboxes $(_mdirs)"
