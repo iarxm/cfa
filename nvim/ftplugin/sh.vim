abbeea4dca1a74c6bdcc4da251b12010e2032c86
Iarom Madden <future@iarom.org>
2023-07-21 13:47:27 +0100

    nvim; qb; minor changes

--- nvim/ftplugin/sh.vim
+++ nvim/ftplugin/sh.vim
@@ -1,3 +1,7 @@
 set tabstop=2
+set shiftwidth=2
+set expandtab
+
+"set tabstop=2
 "set softtabstop=0
 "set shiftwidth=0
--- nvim/lua/plugins/xneotree.lua
+++ nvim/lua/plugins/xneotree.lua
@@ -1,5 +1,5 @@
 return { "nvim-neo-tree/neo-tree.nvim",
-	event = 'VeryLazy',
+    -- event = 'VeryLazy',
     branch = "v2.x",
     dependencies = {
       "nvim-lua/plenary.nvim",
@@ -201,7 +201,8 @@ return { "nvim-neo-tree/neo-tree.nvim",
           follow_current_file = false, -- This will find and focus the file in the active buffer every
                                        -- time the current file is changed while the tree is open.
           group_empty_dirs = false, -- when true, empty folders will be grouped together
-          hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
+          hijack_netrw_behavior = "open_current",
+          --hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
                                                   -- in whatever position is specified in window.position
                                 -- "open_current",  -- netrw disabled, opening a directory opens within the
                                                   -- window like netrw would, regardless of window.position
