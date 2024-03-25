2cdee3a90b61e130ba3f343fdf0ece0648b47daa
Iarom Madden <future@iarom.org>
2024-03-25 12:05:39 +0000

    v: plug scrub. map fix

--- nvim/lua/init/map.lua
+++ nvim/lua/init/map.lua
@@ -1,32 +1,26 @@
--- mappings ###############################################
-local map = vim.api.nvim_set_keymap
-local usr_cmd = vim.api.nvim_create_user_command
-local auc	= vim.api.nvim_create_autocmd
-local aug = vim.api.nvim_create_augroup
-local o_done = "$HOME/dd/a/ozd"
-vim.g.mapleader = ' '
+local map            = vim.api.nvim_set_keymap
+local usr_cmd        = vim.api.nvim_create_user_command
+local auc	           = vim.api.nvim_create_autocmd
+local aug            = vim.api.nvim_create_augroup
+local o_done         = "$HOME/daa/a/ozd"
+vim.g.mapleader      = ' '
 vim.g.maplocalleader = '.'
--- fm 
-map('n', '<leader>sv', 	':wa <bar> so $MYVIMRC<CR>', { noremap = true })
-map('n', '<leader>t', 	':w<CR>:! sh %<CR>', { noremap = true })
-map('n', ';x', 		':! chmod +x %<CR>', { noremap = true })
---## nav ##################################################
-map('n', ';j', '100j', { noremap = true })
-map('n', ';k', '100k', { noremap = true })
-map('n', '<M-j>', '10j', { noremap = true })
-map('n', '<M-k>', '10k', { noremap = true })
-map('n', '<M-h>', '5b', { noremap = true })
-map('n', '<M-l>', '5w', { noremap = true })
-map('n', '<M-w>', '5w', { noremap = true })
-map('n', '<M-;>', '5w', { noremap = true })
+map('n', '<leader>sv', 	':wa <bar> so $MYVIMRC<CR>', { noremap = true }) -- fw
+map('n', '<leader>t', 	':w<CR>:! sh %<CR>',         { noremap = true })
+map('n', ';x', 		':! chmod +x %<CR>',         { noremap = true })
+map('n', ';j',     '100j', { noremap = true }) -- nav ###############################
+map('n', ';k',     '100k', { noremap = true })
+map('n', '<M-j>',  '10j', { noremap = true })
+map('n', '<M-k>',  '10k', { noremap = true })
+map('n', '<M-h>',  '5b', { noremap = true })
+map('n', '<M-l>',  '5w', { noremap = true })
+map('n', '<M-w>',  '5w', { noremap = true })
+map('n', '<M-;>',  '5w', { noremap = true })
 map('n', '<C-w>w', '<C-w>w 100w', { noremap = true }) -- silent?
-map('n', 'd/', 'F/jli', { noremap = true })
--- input map
-map('i', 'f/', '<ESC>F/jli', { noremap = true })
+map('n', 'd/',     'F/jli', { noremap = true })
+map('i', 'f/', '<ESC>F/jli', { noremap = true }) -- input map
 map('i', 'jk', '<ESC>', { noremap = true })
---## nav. pane manage #######################################
--- rearrange
-map('n', '<leader>j', 	'<C-w>J', { noremap = true })
+map('n', '<leader>j', 	'<C-w>J', { noremap = true }) --## nav. pane manage ###########
 map('n', '<leader>k', 	'<C-w>K', { noremap = true })
 map('n', '<leader>h', 	'<C-w>H', { noremap = true })
 map('n', '<leader>l', 	'<C-w>L', { noremap = true })
@@ -35,31 +29,25 @@ map('n', '<C-j>', 		'<C-w>j', 		{ noremap = true })
 map('n', '<C-K>', 		'<C-w>k', 		{ noremap = true })
 map('n', '<C-H>', 		'<C-w>h', 	{ noremap = true })
 map('n', '<C-L>', 		'<C-w>l', { noremap = true })
--- resize
-map('n', '<leader>z', '<C-w>>', { noremap = true })
-map('n', '<leader>x', '<C-w><', { noremap = true })
-map('n', '<leader>v', '<C-w>v<C-l>:e', { noremap = true })
-map('n', '<leader>s', '<C-w>s :e', { noremap = true })
-map('n', ';e', ':call mdx#EqualWidth()<CR>', { noremap = true })
--- nav. tab mang
-map('n', ';t', ':tabnew ', { noremap = true })
+map('n', '<leader>z',    '<C-w>>', { noremap = true }) -- resize
+map('n', '<leader>x',    '<C-w><', { noremap = true })
+map('n', '<leader>v',    '<C-w>v<C-l>:e', { noremap = true })
+map('n', '<leader>s',    '<C-w>s :e', { noremap = true })
+map('n', ';e',           ':call mdx#EqualWidth()<CR>', { noremap = true })
+map('n', ';t', ':tabnew ', { noremap = true }) -- nav. tab mang
 map('n', ';n', ':tabnew <bar> tabprevious <bar> wq<CR>', { noremap = true })
 map('n', 'K', 'gT', { noremap = true })
 map('n', 'J', 'gt', { noremap = true })
 -- map('n', ';f', ':e ', { noremap = true })
---## BUFFERS #############################################
--- bufs
-map('n', 'B', ':e #<CR>', { noremap = true })
-map('n', '<leader>b', ':ls<CR>:b<space>', { noremap = true })
-map('n', ';b', ':bw<CR>', { noremap = true })
--- save and quit - how to do silent???
-map('n', '<leader>Q', ':q<CR>', { noremap = true })
-map('n', '<leader>q', ':wa <bar> q<CR>', { noremap = true })
-map('n', ';w', ':wa<CR>', { noremap = true })
-map('n', '<leader>aq', ':wa <bar> qa<CR>', { noremap = true })
--- open/edit
-map('n', 'E', ':e ', { noremap = true })
-map('n', '<leader>e', ':e ', { noremap = true })
+map('n', 'B',          ':e #<CR>',           { noremap = true }) --bufs #############################
+map('n', '<leader>b',  ':ls<CR>:b<space>',   { noremap = true })
+map('n', ';b',         ':bw<CR>',            { noremap = true })
+map('n', '<leader>Q',  ':q<CR>',             { noremap = true }) -- save and quit - how to do silent???
+map('n', '<leader>q',  ':wa <bar> q<CR>',    { noremap = true })
+map('n', ';s',         ':wa<CR>',            { noremap = true })
+map('n', '<leader>aq', ':wa <bar> qa<CR>',   { noremap = true })
+map('n', 'E',          ':e ',                { noremap = true }) -- open/edit
+map('n', '<leader>e',  ':e ',                { noremap = true })
 -- based on plugins #######################################
 map('n', '<leader>l', ':Limelight<CR>', { noremap = true })
 map('n', '<leader>L', ':Limelight!<CR>', { noremap = true })
@@ -70,23 +58,16 @@ usr_cmd('Zen', 'ZenMode', {})
 map('n', ';z', ':ZenMode<CR>', { noremap = true })
 map('n', '<leader>nn', ':NnnPicker<CR>', { noremap = true })
 map('n', '<leader>nc', ':NnnPicker %:p:h<CR>', { noremap = true })
--- cust
-map('n', '<S-h>', ':call mdx#ToggleHiddenAll()<CR>', { noremap = true })
+map('n', '<S-h>', ':call mdx#ToggleHiddenAll()<CR>', { noremap = true }) --cust
 map('n', ';h', ':call mdx#ToggleWrite()<CR>', { noremap = true })
 map('n', '.n', ':call mdx#WideMargin()<CR>', { noremap = true })
--- syntax ##################################################
--- tags
-map('n', '<leader>st', ':CtrlPTag<CR>', { noremap = true })
--- markdown - compile
--- make scripts
-map('n', ';m', ':!makeu "$PWD" <CR>', { noremap = true })
+map('n', '<leader>st', ':CtrlPTag<CR>', { noremap = true }) -- tags
+map('n', ';m', ':!makeu "$PWD" <CR>', { noremap = true }) -- make scripts
 map('n', ';M', ':make &<CR><CR>', { noremap = true })
--- shell scripts
-map('n', ';c', ':silent !xelatex "%s" &<CR>:redraw!<CR>', { noremap = true })
+map('n', ';c', ':silent !xelatex "%s" &<CR>:redraw!<CR>', { noremap = true }) -- scripts
 map('n', ';p', ':!pda "%"<CR>', { noremap = true })
--- syntax
-map('n', ';l', ':TOC<CR>', { noremap = true })
-map('n', ';s',
+map('n', ';l', ':TOC<CR>', { noremap = true }) -- syntax
+map('n', ';f',
 	':if exists("g:syntax_on") <Bar> ' ..
 	'syntax off <Bar> ' ..
 	'else <Bar> ' ..
@@ -104,27 +85,20 @@ map('n', '.ssa',
 map('n', '.ssb',
 	'i<!-----------><ESC>',
 	{ noremap = true })
--- md .. time / date / ft
-map('n', '.sdd',  ':put =strftime(\'# %d/%m/%y/+\')<ESC>', { noremap = true })
+map('n', '.sdd',  ':put =strftime(\'# %d/%m/%y/+\')<ESC>',     { noremap = true }) -- md .. time / date / ft
 map('n', '.sda',  ':put =strftime(\'**%y/%m/%d %a+**\')<ESC>', { noremap = true })
-map('n', '.smn',  'i<!--- vim:set ft=markdown: ---><ESC>', { noremap = true })
-map('n', '.smt',  'i<!--- vim:set ft=markdownt: ---><ESC>', { noremap = true })
--- oo .. move to end
-map('n', 'me',    ':m $<CR>', { noremap = true })
-map('v', 'me',    ':\'<,>\'m $<CR>', { noremap = true })
--- oo .. move to done file
-map('n', 'md',    '0v$:w! >>' .. o_done .. '<CR>d', { noremap = true })
-map('v', 'md',    ':w! >> ' .. o_done .. '<CR>gvd', { noremap = true })
--- oo .. move across panes
-map('v', 'mr',    'x<C-L>Gp<C-H>', { noremap = true })
--- oo
-map('n', '.oo', ':call oo#OoPanel(\" ', { noremap = true })
-map('n', '.oww', ':call oo#OoPanel("dsiwg/")<CR>', { noremap = true })
-map('n', '.owf', ':call oo#OoPanel("dsiwg/fund.")<CR>', { noremap = true })
--- or use 'gx' - g:netrw_browsex_viewer
+map('n', '.smn',  'i<!--- vim:set ft=markdown: ---><ESC>',     { noremap = true })
+map('n', '.smt',  'i<!--- vim:set ft=markdownt: ---><ESC>',    { noremap = true })
+map('n', 'me',    ':m $<CR>',                                  { noremap = true }) -- oo move to end
+map('v', 'me',    ':\'<,>\'m $<CR>',                           { noremap = true })
+map('n', 'md',    '0v$:w! >>' .. o_done .. '<CR>d',            { noremap = true }) -- oo move to done
+map('v', 'md',    ':w! >> '   .. o_done .. '<CR>gvd',          { noremap = true })
+map('v', 'mr',    'x<C-L>Gp<C-H>',                             { noremap = true }) -- oo .. move across panes
+map('n', '.oo',   ':call oo#OoPanel(\" ', { noremap = true }) -- oo
+map('n', '.oww',  ':call oo#OoPanel("dsiwg/")<CR>', { noremap = true })
+map('n', '.owf',  ':call oo#OoPanel("dsiwg/fund.")<CR>', { noremap = true }) -- or use 'gx' - g:netrw_browsex_viewer
 -- TableMode and Markdown Abbreviations ######################################
--- compact the below into 'mdx' itself.
-map('i', '<expr> <bar><bar>',
+map('i', '<expr> <bar><bar>', -- compact the below into 'mdx' itself.
 	'mdx#isAtStartOfLine("\\|\\|") ? "<c-o>:TableModeEnable<cr><bar><space><bar><left><left>" : "<bar><bar>"',
 	{ silent = true })
 map('i', '<expr> __',
@@ -191,5 +165,4 @@ auc("TabEnter", {
 auc("VimEnter", {
 	group = "WORKING",
 	command = "call uix#ToggleSlineTrig()"})
-
 
--- nvim/lua/plug/inix.lua
+++ nvim/lua/plug/inix.lua
@@ -1,33 +1,4 @@
--- TODO:
--- ANALYZE ASTROVIM
--- https://astronvim.com/Recipes/black_belt
--- https://github.com/ThePrimeagen/harpoon
--- 
--- DEBUGGING/TESTING
--- https://github.com/michaelb/sniprun
--- https://github.com/arjunmahishi/flow.nvim (snippets)
--- https://github.com/nvim-neotest/neotest
-
-
 return {
-  { "powerman/vim-plugin-AnsiEsc" },
-  { "scateu/tsv_edl.vim", cmd = "Edl",}, --exp?
-  { "folke/lazy.nvim", version = "*" }, -- focus?
-  -- { "LazyVim/LazyVim", config = true,},
-    -- ##################################################
-  -- ## WRITING MODE ##################################
-  -- TODO: switch limelight folke/twilight.nvim - based on treesitter
-  {'junegunn/limelight.vim', cmd = 'Limelight'},
-  {'bilalq/lite-dfm', cmd = 'LiteDFMToggle' },
-  {'junegunn/goyo.vim', cmd = 'Goyo' },
-  {'https://github.com/Dotrar/nvim-writeonly'}, -- write only mode (forced insert)
-  -- ## distraction free ###############################
-  --{'honza/writer.vim'},
-  --{'mikewest/vimroom', { 'on': 'VimroomToggle' }
-  --{'amix/vim-zenroom'
-  --{'amix/vim-zenroom2'
-  {'gabenespoli/vim-mutton', cmd = 'MuttonToggle' },
-  -- ## task management ##################################
   {'blindFS/vim-taskwarrior', 
     lazy = true,
     cmd = 'TW',
@@ -42,82 +13,99 @@ return {
     },
   },
   {'vimwiki/vimwiki',
-    lazy = true
-  },
-  -- { 'blakesweeney/unite-taskwarrior'}, "very interesting tool?
-  -- {'itchyny/calendar.vim', cmd = 'Calendar'},
-  -- Session Management
-  {'tpope/vim-obsession', cmd = 'Obsession'},
-  -- File Explorers
-  {'cocopon/vaffle.vim', cmd = 'Vaffle'},
-  {'mcchrish/nnn.vim', cmd = 'NnnExplorer'},
-  -- {'/usr/bin/fzf'}, -- Specify path to fzf bin
+    lazy = true,
+    ft = "wiki"
+  }, 
+  { "powerman/vim-plugin-AnsiEsc" },
+  { "scateu/tsv_edl.vim",      cmd = "Edl",},        --exp?
+  { "folke/lazy.nvim",         version = "*" },              -- focus?
+    {'junegunn/limelight.vim', cmd = 'Limelight'},   -- focus/write
+  {'bilalq/lite-dfm',          cmd = 'LiteDFMToggle' },   -- focus/write
+  {'junegunn/goyo.vim',        cmd = 'Goyo' },       -- focus/write
+  {'https://github.com/Dotrar/nvim-writeonly'},      -- write only mode (forced insert)
+  {'gabenespoli/vim-mutton',   cmd = 'MuttonToggle' }, -- focus/write
+  {'tpope/vim-obsession',      cmd = 'Obsession'},   -- Session Management
+  {'cocopon/vaffle.vim',       cmd = 'Vaffle'},      -- fm
+  {'mcchrish/nnn.vim',         cmd = 'NnnExplorer'}, --fm
   {'junegunn/fzf'}, -- which fzf??
-  -- Syntax
-  {'godlygeek/tabular', cmd = 'Tabularize'},
-  {'vim-pandoc/vim-rmarkdown', ft = 'rmarkdown'},
-  {'jalvesaq/Nvim-R', ft = 'rmarkdown'},
-  {'vim-pandoc/vim-criticmarkup'},
-  {'manasthakur/vim-asyncmake', cmd = 'AsyncMake'},
+  {'godlygeek/tabular',        cmd = 'Tabularize'},  -- syn
+  {'vim-pandoc/vim-rmarkdown', ft = 'rmarkdown'},    -- syn
+  {'jalvesaq/Nvim-R',          ft = 'rmarkdown'},    -- syn
+  {'vim-pandoc/vim-criticmarkup'},                   -- syn
+  {'manasthakur/vim-asyncmake', cmd = 'AsyncMake'},  -- syn
+  {'tpope/vim-surround'}, -- func
+  {'tmsvg/pear-tree'},    -- func
+  {'elzr/vim-json'},      -- func
+  {'sirver/ultisnips'},   -- func
+  {'honza/vim-snippets'}, -- func
+  {'junegunn/vim-easy-align'},
+  {'vim-test/vim-test',          cmd = 'TestNearest'},
+  {'vim-scripts/SyntaxRange'},
+  {'dhruvasagar/vim-table-mode', cmd = 'TableModeEnable' },
+  {'vim-airline/vim-airline',    cmd = 'AirlineToggle'},
+  {'sotte/presenting.vim',       cmd = 'PresentingStart'},
+  {'rdnetto/YCM-Generator',      branch = 'stable'},
+  {'fatih/vim-go',               ft = 'go'},
+  {'nsf/gocode', tag = 'v.20150303', rtp = 'vim', ft = 'go'},
+  {'vim-voom/VOoM',              cmd = 'VoomToggle'},
+  {'tpope/vim-speeddating'},
+  {'universal-ctags/ctags'},                       -- tag
+  {'majutsushi/tagbar',          cmd = 'Tagbar'},  -- tag
+  {'vim-scripts/taglist.vim',    cmd = 'Tlist'},   -- tag
+  {'ctrlpvim/ctrlp.vim'},    -- search
+  {'vim-scripts/utl.vim'},   -- ?
+  { "tpope/vim-repeat", event = "VeryLazy" }, -- makes plugs dot-repeatable like leap
+  {'JamshedVesuna/vim-markdown-preview', cmd = 'MdPreview'},
+  {'dkarter/bullets.vim'},
+  {'felipec/notmuch-vim', cmd = 'NotMuch'},
+  {'pseewald/vim-anyfold', cmd = 'AnyFoldActivate'},
+  {'Konfekt/FastFold'},
+  { "dstein64/vim-startuptime",
+  	cmd = "StartupTime",
+    	config = function() vim.g.startuptime_tries = 10 end},
+  -- TODO:
+  -- ANALYZE ASTROVIM
+  -- https://astronvim.com/Recipes/black_belt
+  -- https://github.com/ThePrimeagen/harpoon
+  -- DEBUGGING/TESTING
+  -- https://github.com/michaelb/sniprun
+  -- https://github.com/arjunmahishi/flow.nvim (snippets)
+  -- https://github.com/nvim-neotest/neotest
+  -- { "LazyVim/LazyVim", config = true,},
+  -- TODO: switch limelight folke/twilight.nvim - based on treesitter
+     -- ######################################################
+  -- 'Shougo/vimproc.vim', {'do' : 'make'} " unite taskwarrior dep.
+  -- "Plug 'shougo/unite.vim' "unite tw dep
+  -- "Plug 'Shougo/denite.nvim' "unite replacement
+  --{ "nvim-lua/plenary.nvim", lazy = true },   -- library used by other plugins
+  -- { 'blakesweeney/unite-taskwarrior'}, "very interesting tool?
+  -- {'/usr/bin/fzf'}, -- Specify path to fzf bin
   -- 'vim-pandoc/vim-pandoc', { 'for': ['markdown','tex'] }
   -- 'vim-pandoc/vim-pandoc', { 'for': ['tex'] }
   -- 'vim-pandoc/vim-pandoc-after'
   -- 'vim-pandoc/vim-pandoc-syntax'
   -- 'preservim/vim-markdown'
   -- 'vim-pandoc/vim-markdownfootnotes'
+  -- "Plug 'mgor/vim-markdown-grip'
   -- 'xuhdev/vim-latex-live-preview', { 'for': 'tex' }
   -- 'conornewton/vim-latex-preview'
   -- 'yegappan/asyncmake' "errors
-  -- Functionality ####################################
-  {'tpope/vim-surround'},
-  {'tmsvg/pear-tree'},
-  {'elzr/vim-json'},
-  {'sirver/ultisnips'},
-  {'honza/vim-snippets'},
-  {'junegunn/vim-easy-align'},
-  {'vim-test/vim-test', cmd = 'TestNearest'},
-  {'vim-scripts/SyntaxRange'},
-  {'dhruvasagar/vim-table-mode', cmd = 'TableModeEnable' },
-  -- Others (Add more as needed)
-  -- 'Shougo/vimproc.vim', {'do' : 'make'} " unite taskwarrior dep.
-  -- "Plug 'shougo/unite.vim' "unite tw dep
-  -- "Plug 'Shougo/denite.nvim' "unite replacement
-  {'vim-airline/vim-airline', cmd = 'AirlineToggle'},
-  {'sotte/presenting.vim', cmd = 'PresentingStart'},
-  {'rdnetto/YCM-Generator', branch = 'stable'},
-  {'fatih/vim-go', ft = 'go'},
-  {'nsf/gocode', tag = 'v.20150303', rtp = 'vim', ft = 'go'},
-  {'vim-voom/VOoM', cmd = 'VoomToggle'},
-  {'tpope/vim-speeddating'},
   -- 'chrisbra/NrrwRgn'
   -- "Plug 'https://github.com/ludovicchabant/vim-gutentags'
-  -- Tags
-  {'universal-ctags/ctags'},
-  {'majutsushi/tagbar', cmd = 'Tagbar'},
-  {'vim-scripts/taglist.vim', cmd = 'Tlist'},
-  -- Search
-  {'ctrlpvim/ctrlp.vim'},
-  {'tpope/vim-repeat'},
-  {'vim-scripts/utl.vim'},
+  -- ####################################################
   -- "Plug 'jremmen/vim-ripgrep'
   -- "Plug 'vim-scripts/ctags.vim'
   -- "Plug 'junegunn/vim-github-dashboard.git'
   -- "Plug 'rktjmp/git-info.vim'
   -- "Plug 'mhinz/vim-startify'
   -- "Plug 'hsitz/VimOrganizer'
-  -- New Plugins
-  {'JamshedVesuna/vim-markdown-preview', cmd = 'MdPreview'},
-  -- "Plug 'mgor/vim-markdown-grip'
-  {'dkarter/bullets.vim'},
-  {'felipec/notmuch-vim', cmd = 'NotMuch'},
-  {'pseewald/vim-anyfold', cmd = 'AnyFoldActivate'},
-  {'Konfekt/FastFold'},
-	-- "Plug 'kurkale6ka/vim-chess'
-  { "dstein64/vim-startuptime",
-  	cmd = "StartupTime",
-    	config = function() vim.g.startuptime_tries = 10 end},
-  { "nvim-lua/plenary.nvim", lazy = true }, -- library used by other plugins
-  { "tpope/vim-repeat", event = "VeryLazy" }, -- makes some plugins dot-repeatable like leap
--- END
+  -- {'itchyny/calendar.vim', cmd = 'Calendar'},
+  -- #######################################################
+  --{'honza/writer.vim'}, -- focus
+  --{'mikewest/vimroom', { 'on': 'VimroomToggle' } -- focus
+  --{'amix/vim-zenroom' -- focus
+  --{'amix/vim-zenroom2' -- focus
+
+  -- "Plug 'kurkale6ka/vim-chess'
 }
 
