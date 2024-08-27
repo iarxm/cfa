c4af10f5d1f0e0260434a4a51564dddcae22ad3d
Iarom Madden <future@iarom.org>
2024-08-27 01:42:29 +0100

    mlx: mutt to quteb ipc bridge

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,2 +1,4 @@
+# mail configs
 config.source('configa.py')
+c.content.local_content_can_access_remote_urls = True
 
---
+++ qutebrowser/configm.py
@@ -0,0 +1,5 @@
+# vim:fenc=utf-8:nu:ai:si:et:ts=4:sw=4:ft=python:
+# AUTHOR: IAROM MADDEN
+config.load_autoconfig() # req for site specifics
+config.source('configx.py')
+config.source('userinterfa/colaaa.py')
