c26ca9ce7b86d718d325948c59c6decb97d85f95
Iarom Madden <future@iarom.org>
2024-09-05 13:45:55 +0100

    nvim: ufo key binds mod

--- nvim/lua/plug/ufo.lua
+++ nvim/lua/plug/ufo.lua
@@ -7,8 +7,8 @@ return {
         vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
         vim.o.foldlevelstart = 99
         vim.o.foldenable = true
-        vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-        vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+        vim.keymap.set('n', 'zr', require('ufo').openAllFolds)
+        vim.keymap.set('n', 'zm', require('ufo').closeAllFolds)
     end,
     config = function()
         require('ufo').setup( {
