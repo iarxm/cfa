034e2ba6e39a14cf1c4cb63f5d961d249da2d150
Iarom Madden <mail@iarom.org>
2025-02-27 14:59:18 +0000

    nvim: colorscheme & neogit add

--- nvim/lua/profa/init.lua
+++ nvim/lua/profa/init.lua
@@ -1,6 +1,6 @@
---vim.env.COLORS_VIM       = "kanagawa-wave"
+vim.env.COLORS_VIM       = "kanagawa-wave"
 --vim.env.COLORS_VIM       = "kanagawa"
-vim.env.COLORS_VIM       = "rosebones"
+--vim.env.COLORS_VIM       = "rosebones"
 vim.env.COLORS_BLACK     = "colors/black.vim"
 vim.g.netrw_banner       = 0
 vim.g.netrw_liststyle    = 0
