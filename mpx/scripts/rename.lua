45c31b36843e5e2fc481e3cde232774d84ff5130
Iarom Madden <mail@iarom.org>
2025-04-10 23:46:49 +0100

    mpv: cleanup; experiment with self contained analysis

--- mpx/scripts/rename.lua
+++ mpx/scripts/rename.lua
@@ -5,6 +5,7 @@ local utils = require "mp.utils"
 package.path = mp.command_native({"expand-path", "~~/scripts/a-modules/?.lua;"})..package.path
 local input = require "user-input-module"
 
+
 local function rename(text, error)
     if not text then return msg.warn(error) end
 
@@ -19,27 +20,31 @@ local function rename(text, error)
     local success, error = os.rename(filepath, newfilepath)
     if not success then msg.error(error) end
 
-    -- adding the new path to the playlist, and restarting the file with the correct path
+    -- adding new path to playlist; restarting file with correct path
     mp.commandv("loadfile", newfilepath, "append")
     mp.commandv("playlist-move", mp.get_property_number("playlist-count", 2) - 1, mp.get_property_number("playlist-pos", 1) + 1)
     mp.commandv("playlist-remove", "current")
 end
 
---if the file closes while renaming then cancel the operation
---this removes any ambiguity over which file will be renamed
+
 mp.register_event("end-file", function()
+    --if file closes while renaming then cancel
     input.cancel_user_input()
 end)
 
-mp.add_key_binding("F2", "rename-file", function()
-    filepath = mp.get_property('path')
-    directory, filename = utils.split_path(filepath)
-    input.cancel_user_input()
-    input.get_user_input(rename, {
-        text = "Enter new filename:",
-        default_input = filename,
-        replace = false,
-        cursor_pos = filename:find("%.%w+$")
+
+mp.add_key_binding("r", "rename-file",
+    function()
+    
+        filepath = mp.get_property('path')
+        directory, filename = utils.split_path(filepath)
+        input.cancel_user_input()
+
+        input.get_user_input(rename, {
+            text = "Enter new filename:",
+            default_input = filename,
+            replace = false,
+            cursor_pos = filename:find("%.%w+$")
     })
 end)
 
