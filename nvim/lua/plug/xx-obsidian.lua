8df5bcd8e3afd4a663dac9ffd4b87ed6e262cac8
Iarom Madden <mail@iarom.org>
2025-04-11 22:34:22 +0100

    m

--- nvim/lua/plug/xx-obsidian.lua
+++ nvim/lua/plug/xx-obsidian.lua
@@ -37,18 +37,18 @@ return {
             vim.ui.open(url) -- need Neovim 0.10.0+
         end,
         --
+--󰄰 
         ui = {
             checkboxes = {
               -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
-                --[" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
                 [" "] = { char = "☐", hl_group = "ObsidianTodo" },
                 ["x"] = { char = "", hl_group = "ObsidianDone" },
                 [">"] = { char = "", hl_group = "ObsidianRightArrow" },
                 ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
                 ["!"] = { char = "", hl_group = "ObsidianImportant" },
               -- Replace the above with this if you don't have a patched font:
-              -- [" "] = { char = "☐", hl_group = "ObsidianTodo" },
-              -- ["x"] = { char = "✔", hl_group = "ObsidianDone" },
+              --[" "] = { char = "☐", hl_group = "ObsidianTodo" },
+              --["x"] = { char = "✔", hl_group = "ObsidianDone" },
             -- You can also add more custom ones...
             },
             hl_groups = {
