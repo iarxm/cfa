12486a5d0554f7c08fc068e39b19e2427691ac54
Iarom Madden <mail@iarom.org>
2025-11-26 11:43:54 +0000

    qb: style name scrubs; disable outlook greasemonkeys

--- qutebrowser/styles/stylemap.py
+++ qutebrowser/styles/stylemap.py
@@ -1,5 +1,5 @@
 styles = {
     #'qute.help.css': ['qute://help*', '*someotherdomain.xyz*'],
     #'chat.openai.com.user.css': ['*chat.openai.com*', 'https://chat.openai.com*'],
-    'chat.openai.com2.user.css': ['*chat.openai.com*']
+    'chat-openai-com_2-user.css': ['*chat.openai.com*']
 }
