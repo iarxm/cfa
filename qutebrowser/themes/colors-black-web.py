79a0310dd9484eb68c29c794f9b45ac4b7dcac6e
Iarom Madden <mail@iarom.org>
2026-02-24 02:32:14 +0000

    qb: updating black color css pointers

--- qutebrowser/themes/colors-black-web.py
+++ qutebrowser/themes/colors-black-web.py
@@ -1,9 +1,8 @@
+import os
+HOME                                             = os.getenv("HOME")
 c.colors.webpage.darkmode.enabled                = True
 c.colors.webpage.darkmode.policy.images          = "never"
 c.colors.webpage.preferred_color_scheme          = "dark"
 c.colors.webpage.darkmode.contrast               = 0.0
 c.colors.webpage.bg                              = "black"
-import os
-HOME                       = os.getenv("HOME")
 c.content.user_stylesheets = [ os.getenv('WINDOW_ELEMENTS'), os.getenv('COLORS_BLACKX') ]
-
